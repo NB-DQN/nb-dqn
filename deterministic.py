7a9c670d6c24feaf0b48298b0b3aba5cf8b26d4c
iTakeshi <takeshi.ito.doraemon@gmail.com>
2015-09-16 23:13:01 +0900

    deterministic agent works fine

--- deterministic.py
+++ deterministic.py
@@ -2,14 +2,24 @@ import modules
 
 import copy
 
-size = (5, 5)
+size = (9, 9)
 environment = modules.environment.Environment(size)
 place_cell = modules.place_cell.DeterministicPlaceCell(size)
 agent = modules.agent.DeterministicAgent(place_cell)
 
 environment.maze.display_cui()
 
-while not environment.exit():
-    agent.set_wall(environment.wall())
-    environment.move(agent.choose_action())
-    print(environment.current_coordinate)
+f = open('deterministic.log', 'w')
+
+try:
+    while not environment.exit():
+        agent.set_wall(environment.wall())
+        environment.move(agent.choose_action())
+        f.write(str(environment.current_coordinate))
+        f.write('\n')
+except KeyboardInterrupt:
+    print(agent.wall_information)
+    print(agent.place_cell.history)
+    print(environment.history)
+
+f.close()
