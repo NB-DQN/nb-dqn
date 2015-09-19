9de01bae5937b9d5478c8687c131956303210b58
iTakeshi <takeshi.ito.doraemon@gmail.com>
2015-09-19 22:41:40 +0900

    dump log file

--- deterministic.py
+++ deterministic.py
@@ -11,11 +11,18 @@ environment.maze.display_cui()
 
 f = open('deterministic.log', 'w')
 
+f.write(",".join(str(i) for i in size))
+f.write("\n")
+f.write(environment.maze.dump_params())
+f.write("\n")
+f.write("0,0")
+f.write("\n")
+
 try:
     while not environment.exit():
         agent.set_wall(environment.wall())
         environment.move(agent.choose_action())
-        f.write(str(environment.current_coordinate))
+        f.write(",".join(str(i) for i in environment.current_coordinate))
         f.write('\n')
 except KeyboardInterrupt:
     print(agent.wall_information)
