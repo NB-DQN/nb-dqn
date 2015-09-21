0dcf7fce99915d18cd6b81043ff09eade3be5751
iTakeshi <takeshi.ito.doraemon@gmail.com>
2015-09-22 02:49:10 +0900

    visual

---
+++ visual.py
@@ -0,0 +1,34 @@
+import modules
+
+import copy
+
+size = (9, 9)
+environment = modules.environment.Environment(size)
+place_cell = modules.place_cell.VisualPlaceCell(size)
+agent = modules.agent.DeterministicAgent(place_cell)
+
+environment.maze.display_cui()
+
+f = open('visual.log', 'w')
+
+f.write(",".join(str(i) for i in size))
+f.write("\n")
+f.write(environment.maze.dump_params())
+f.write("\n")
+f.write("0,0")
+f.write("\n")
+
+try:
+    while not environment.exit():
+        agent.set_wall(environment.wall())
+        action = agent.choose_action()
+        agent.place_cell.move(action, environment.visual_image())
+        environment.move(action)
+        f.write(",".join(str(i) for i in environment.current_coordinate))
+        f.write('\n')
+except KeyboardInterrupt:
+    print(agent.wall_information)
+    print(agent.place_cell.history)
+    print(environment.history)
+
+f.close()
